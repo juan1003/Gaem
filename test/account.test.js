4edc313f88e8d747566b1345d5796074e887c271
Juan De Jesus <jdejesus93@gmail.com>
2022-01-09 14:21:46 -0600

    WIP

--- test/account.test.js
+++ test/account.test.js
@@ -1,25 +1,23 @@
-const { ObjectId } = require('mongodb')
+const { hash } = require('bcrypt')
 const accountRepository = require('../src/repositories/account')
 
 describe('User repos', () => {
-    let accounts, account1;
+    let accounts;
 
     beforeEach(async () => {
-        account1 = await accountRepository.add({
+        await accountRepository.add({
             username: 'jdejesus',
             password: 'pass1234'
         })
 
         accounts = await accountRepository.get()
-        console.log(accounts)
     })
 
     afterEach(async () => {
-        console.log(account1)
-        // await accountRepository.remove(ObjectId(account1._id))
+        await accountRepository.remove(ObjectId(account1._id))
     })
 
-    it('should return an empty array', () => {
+    it('should return 1 account', () => {
         expect(accounts.length).toBe(1)
     })
 })
